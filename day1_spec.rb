94e87d0c8ef62ede16111fdcbecf991aadb9b68f
Mike Dalessio <mike.dalessio@gmail.com>
2016-12-02 20:03:42 -0500

    day 1 star 2

--- day1_spec.rb
+++ day1_spec.rb
@@ -1,4 +1,5 @@
 require 'rspec'
+require 'set'
 
 class Position
   HEADINGS = [
@@ -10,24 +11,34 @@ class Position
 
   attr :x, :y, :heading
 
-  def initialize
-    @x = 0
-    @y = 0
+  def initialize x = 0, y = 0
+    @x = x
+    @y = y
     @heading = NORTH
   end
 
   def move step
+    move_path step
+    self
+  end
+
+  def move_path step
     match = /([LR])([0-9]+)/.match(step)
     turn match[1]
-    walk match[2].to_i
-    self
+
+    [].tap do |path|
+      match[2].to_i.times do
+        walk 1
+        path << coordinates
+      end
+    end
   end
 
   def taxicab_geometry
     x.abs + y.abs
   end
 
-  def position
+  def coordinates
     [x, y]
   end
 
@@ -71,10 +82,14 @@ describe Position do
   end
 
   context "distance" do
-    it { expect(Position.new.move("R2").position).to eq([2, 0]) }
-    it { expect(Position.new.move("R0").move("R2").position).to eq([0, -2]) }
-    it { expect(Position.new.move("R0").move("R0").move("R2").position).to eq([-2, 0]) }
-    it { expect(Position.new.move("R0").move("R0").move("R0").move("R2").position).to eq([0, 2]) }
+    it { expect(Position.new.move("R2").coordinates).to eq([2, 0]) }
+    it { expect(Position.new.move("R0").move("R2").coordinates).to eq([0, -2]) }
+    it { expect(Position.new.move("R0").move("R0").move("R2").coordinates).to eq([-2, 0]) }
+    it { expect(Position.new.move("R0").move("R0").move("R0").move("R2").coordinates).to eq([0, 2]) }
+  end
+
+  context "#move_path" do
+    it { expect(Position.new.move_path("R2")).to eq([[1, 0], [2, 0]]) }
   end
 end
 
@@ -85,19 +100,45 @@ class GridPath
     @pathstring = pathstring
   end
 
+  def pathstring_steps
+    pathstring.split(/, ?/)
+  end
+
   def distance
-    pathstring.split(/, ?/).inject(Position.new) do |position, step|
+    pathstring_steps.inject(Position.new) do |position, step|
       position.move step
     end.taxicab_geometry
+  end
+
+  def first_revisit_distance
+    position = Position.new
+    locations = Set.new
+    pathstring_steps.each do |step|
+      path = position.move_path step
+      path.each do |crossing|
+        if locations.include? crossing
+          return Position.new(*crossing).taxicab_geometry
+        end
+        locations.add crossing
+      end
+    end
+    nil
   end
 end
 
 describe GridPath do
-  it { expect(GridPath.new("R2, L3").distance).to eq 5 }
-  it { expect(GridPath.new("R2, R2, R2").distance).to eq 2 }
-  it { expect(GridPath.new("R5, L5, R5, R3").distance).to eq 12 }
+  context "#distance" do
+    it { expect(GridPath.new("R2, L3").distance).to eq 5 }
+    it { expect(GridPath.new("R2, R2, R2").distance).to eq 2 }
+    it { expect(GridPath.new("R5, L5, R5, R3").distance).to eq 12 }
+  end
+
+  context "#first_revisit" do
+    it { expect(GridPath.new("R8, R4, R4, R8").first_revisit_distance).to eq 4 }
+  end
 end
 
 path = "L1, L5, R1, R3, L4, L5, R5, R1, L2, L2, L3, R4, L2, R3, R1, L2, R5, R3, L4, R4, L3, R3, R3, L2, R1, L3, R2, L1, R4, L2, R4, L4, R5, L3, R1, R1, L1, L3, L2, R1, R3, R2, L1, R4, L4, R2, L189, L4, R5, R3, L1, R47, R4, R1, R3, L3, L3, L2, R70, L1, R4, R185, R5, L4, L5, R4, L1, L4, R5, L3, R2, R3, L5, L3, R5, L1, R5, L4, R1, R2, L2, L5, L2, R4, L3, R5, R1, L5, L4, L3, R4, L3, L4, L1, L5, L5, R5, L5, L2, L1, L2, L4, L1, L2, R3, R1, R1, L2, L5, R2, L3, L5, L4, L2, L1, L2, R3, L1, L4, R3, R3, L2, R5, L1, L3, L3, L3, L5, R5, R1, R2, L3, L2, R4, R1, R1, R3, R4, R3, L3, R3, L5, R2, L2, R4, R5, L4, L3, L1, L5, L1, R1, R2, L1, R3, R4, R5, R2, R3, L2, L1, L5"
 
 puts GridPath.new(path).distance
+puts GridPath.new(path).first_revisit_distance
