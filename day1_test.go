ce044ac8cdd18bed96eac519d7cb271288c206d9
Mike Dalessio <mike.dalessio@gmail.com>
2016-12-04 15:24:13 -0500

    day 1 star 2 in golang

--- day1_test.go
+++ day1_test.go
@@ -2,6 +2,7 @@ package adventofcode2016_test
 
 import (
 	"fmt"
+	"github.com/deckarep/golang-set"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"math"
@@ -10,15 +11,20 @@ import (
 	"strings"
 )
 
+var pathSegmentRe = regexp.MustCompile("([LR])([0-9]+)")
+
 type Coordinates struct {
 	x, y int
+}
+
+func (self Coordinates) taxicabGeometry() uint {
+	return uint(math.Abs(float64(self.x)) + math.Abs(float64(self.y)))
 }
 
 var NORTH = Coordinates{0, 1}
 var EAST = Coordinates{1, 0}
 var SOUTH = Coordinates{0, -1}
 var WEST = Coordinates{-1, 0}
-var pathSegmentRe = regexp.MustCompile("([LR])([0-9]+)")
 
 type Position struct {
 	location Coordinates
@@ -29,7 +35,7 @@ func NewPosition() *Position {
 	return &Position{heading: NORTH}
 }
 
-func (self *Position) move(segment string) {
+func (self *Position) move(segment string) []Coordinates {
 	match := pathSegmentRe.FindStringSubmatch(segment)
 	direction := match[1]
 	distance, err := strconv.ParseUint(match[2], 10, 16)
@@ -38,7 +44,13 @@ func (self *Position) move(segment string) {
 	}
 
 	self.turn(direction)
-	self.walk(uint(distance))
+
+	intersections := make([]Coordinates, distance)
+	for j := uint64(0); j < distance; j++ {
+		self.walk(1)
+		intersections[j] = self.location
+	}
+	return intersections
 }
 
 func (self *Position) turn(direction string) {
@@ -73,10 +85,6 @@ func (self *Position) walk(distance uint) {
 	self.location.y += self.heading.y * int(distance)
 }
 
-func (self Position) taxicabGeometry() uint {
-	return uint(math.Abs(float64(self.location.x)) + math.Abs(float64(self.location.y)))
-}
-
 type GridPath struct {
 	path string
 }
@@ -90,7 +98,21 @@ func (self GridPath) distance() uint {
 	for _, segment := range self.segments() {
 		position.move(segment)
 	}
-	return position.taxicabGeometry()
+	return position.location.taxicabGeometry()
+}
+
+func (self GridPath) first_revisit_distance() uint {
+	intersections := mapset.NewSet()
+	position := NewPosition()
+	for _, segment := range self.segments() {
+		for _, intersection := range position.move(segment) {
+			if intersections.Contains(intersection) {
+				return intersection.taxicabGeometry()
+			}
+			intersections.Add(intersection)
+		}
+	}
+	return position.location.taxicabGeometry()
 }
 
 var _ = Describe("Day1", func() {
@@ -140,6 +162,15 @@ var _ = Describe("Day1", func() {
 				position.move("R2")
 				Expect(position.location).To(Equal(Coordinates{0, 0}))
 			})
+
+			It("returns a slice of all intersections spanned by the segment", func() {
+				position := NewPosition()
+				intersections := position.move("R2")
+				Expect(intersections).To(Equal([]Coordinates{
+					Coordinates{1, 0},
+					Coordinates{2, 0},
+				}))
+			})
 		})
 	})
 
@@ -157,13 +188,23 @@ var _ = Describe("Day1", func() {
 				Expect(GridPath{"R5, L5, R5, R3"}.distance()).To(Equal(uint(12)))
 			})
 		})
+
+		Describe("#first_revisit", func() {
+			It("returns the intersection first revisited", func() {
+				Expect(GridPath{"R8, R4, R4, R8"}.first_revisit_distance()).To(Equal(uint(4)))
+			})
+		})
 	})
 
 	Describe("the puzzle", func() {
-		It("star 1", func() {
-			path := "L1, L5, R1, R3, L4, L5, R5, R1, L2, L2, L3, R4, L2, R3, R1, L2, R5, R3, L4, R4, L3, R3, R3, L2, R1, L3, R2, L1, R4, L2, R4, L4, R5, L3, R1, R1, L1, L3, L2, R1, R3, R2, L1, R4, L4, R2, L189, L4, R5, R3, L1, R47, R4, R1, R3, L3, L3, L2, R70, L1, R4, R185, R5, L4, L5, R4, L1, L4, R5, L3, R2, R3, L5, L3, R5, L1, R5, L4, R1, R2, L2, L5, L2, R4, L3, R5, R1, L5, L4, L3, R4, L3, L4, L1, L5, L5, R5, L5, L2, L1, L2, L4, L1, L2, R3, R1, R1, L2, L5, R2, L3, L5, L4, L2, L1, L2, R3, L1, L4, R3, R3, L2, R5, L1, L3, L3, L3, L5, R5, R1, R2, L3, L2, R4, R1, R1, R3, R4, R3, L3, R3, L5, R2, L2, R4, R5, L4, L3, L1, L5, L1, R1, R2, L1, R3, R4, R5, R2, R3, L2, L1, L5"
+		path := "L1, L5, R1, R3, L4, L5, R5, R1, L2, L2, L3, R4, L2, R3, R1, L2, R5, R3, L4, R4, L3, R3, R3, L2, R1, L3, R2, L1, R4, L2, R4, L4, R5, L3, R1, R1, L1, L3, L2, R1, R3, R2, L1, R4, L4, R2, L189, L4, R5, R3, L1, R47, R4, R1, R3, L3, L3, L2, R70, L1, R4, R185, R5, L4, L5, R4, L1, L4, R5, L3, R2, R3, L5, L3, R5, L1, R5, L4, R1, R2, L2, L5, L2, R4, L3, R5, R1, L5, L4, L3, R4, L3, L4, L1, L5, L5, R5, L5, L2, L1, L2, L4, L1, L2, R3, R1, R1, L2, L5, R2, L3, L5, L4, L2, L1, L2, R3, L1, L4, R3, R3, L2, R5, L1, L3, L3, L3, L5, R5, R1, R2, L3, L2, R4, R1, R1, R3, R4, R3, L3, R3, L5, R2, L2, R4, R5, L4, L3, L1, L5, L1, R1, R2, L1, R3, R4, R5, R2, R3, L2, L1, L5"
 
+		It("star 1", func() {
 			fmt.Println("star 1 distance is", GridPath{path}.distance())
+		})
+
+		It("star 2", func() {
+			fmt.Println("star 2 distance is", GridPath{path}.first_revisit_distance())
 		})
 	})
 })
