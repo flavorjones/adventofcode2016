27967f5f191ba5abc92cfe460cc89f26432a0504
Mike Dalessio <mike.dalessio@gmail.com>
2016-12-04 14:53:18 -0500

    bootstrap go / ginkgo

---
+++ adventofcode2016_suite_test.go
@@ -0,0 +1,13 @@
+package adventofcode2016_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestAdventofcode2016(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Adventofcode2016 Suite")
+}
